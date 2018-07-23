477995e091a95c7cca93cb6fd0da73e1cd03e02d
remi <remi.boivin@epitech.eu>
2018-07-23 17:10:33 +0200

    First commit

---
+++ lib/app.rb
@@ -0,0 +1,22 @@
+# coding: utf-8
+require "gossip"
+
+class GossipProjectApp < Sinatra::Base
+
+  get '/gossips/new' do
+    erb:new_gossips
+  end
+  post '/gossips/new' do
+    Gossip.new(params["gossip_author"], params["gossip_content"]).save
+    redirect '/'
+  end
+
+  get '/' do
+    erb :index, locals: {gossips: Gossip.all}
+  end
+
+  get %r{/gossips/([\d]+)} do
+     @id = params[:captures].first
+   end
+
+end
